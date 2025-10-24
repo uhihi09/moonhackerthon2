81ce7ce411df0db26f5b4325bc27f67f663d5028
조상철 <chakanchul2533@gmail.com>
2025-10-25 03:26:56 +0900

    feat: 홈 구현

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://moonhackerthon.onrender.com';
+const API_BASE_URL = 'http://135.04.103.218:8080/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
